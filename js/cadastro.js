8c5659d05b9217fec0b74253176e221ff64eea04
A.Macedo <mariaclaramacedosouza71@gmail.com>
2025-12-02 17:13:05 -0300

    'conclusão' login e cadastro(so falta animação)

---
+++ js/cadastro.js
@@ -0,0 +1,4 @@
+const btnCadastrar = document.getElementById("btn-cadastro");
+btnCadastrar.addEventListener("click", function () {
+    window.location.assign("/pages/home.html")
+})
---
+++ js/script.js
@@ -0,0 +1,8 @@
+const btnLogin = document.getElementById("btn-login");
+btnLogin.addEventListener("click", function () {
+        window.location.assign("/pages/home.html")
+    })
+
+
+
+
